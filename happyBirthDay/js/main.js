b7a2c4218e952374449dd5dc4c8a263140324368
Sang11n <sangkbd01122005@gmail.com>
2025-07-31 22:37:00 +0700

    sn

--- happyBirthDay/js/main.js
+++ happyBirthDay/js/main.js
@@ -2,7 +2,9 @@ const click= document.querySelector('.click');
 const gift= document.querySelector('.gift_box');
 const shadow= document.querySelector('.shadow');
 const letter= document.querySelector('.letter');
-const cake= document.querySelector('.img');
+const cake= document.querySelector('#cake');
+const body= document.querySelector('body');
+
 
 
 
@@ -13,11 +15,14 @@ click.addEventListener('click',()=>{
         setTimeout(()=>click.style.display= "none", 1000);
         setTimeout(()=>gift.style.display= "none", 1000);
         setTimeout(()=>shadow.style.display= "none", 1000);
-        // setTimeout(()=>cake.style.opacity= "1", 1100);
-        setTimeout(()=>letter.style.opacity= "1", 1200);
-        // click.classList.add('img');
+        setTimeout(()=>letter.style.opacity= "1", 1000);
+        setTimeout(()=>body.style.backgroundColor= "#ce7a7aff", 1000);
     }else {
         click.classList.remove('active');
     }
 
 })
+function rain(){
+    let e= document.querySelector('.letter');
+
+}
